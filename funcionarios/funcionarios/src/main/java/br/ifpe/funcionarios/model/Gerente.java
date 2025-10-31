3bc581d0ca899d6ddfb25744f8d8c6c591634dfc
Artur Tome <arturtomex@gmail.com>
2025-10-31 08:42:34 -0300

    Add files via upload

---
+++ funcionarios/funcionarios/src/main/java/br/ifpe/funcionarios/model/Gerente.java
@@ -0,0 +1,26 @@
+package br.ifpe.funcionarios.model;
+public class Gerente extends Funcionarios {
+
+    private double bonus;
+
+    public Gerente(String nome, int idade, String email, int codigo, double salario, String rua, String cidade,
+            double bonus) {
+        super(nome, idade, email, codigo, "Gerente", salario, rua, cidade);
+        this.bonus = bonus;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                ", bonus='" + getBonus() + "'" +
+                "\n***********************************\n";
+    }
+}
